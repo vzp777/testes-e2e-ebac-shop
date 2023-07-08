1eb8ce44a8243c58993437302933913aeacb2506
Victor Zamana Penna <111162621+vzp777@users.noreply.github.com>
2023-07-08 16:18:05 -0300

    Update login.spec.js
    fix "Login.spec.js".

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -10,7 +10,6 @@ context('Funcionalidade Login', () => {
 
     beforeEach(() => {
         cy.visit('minha-conta')
-        cy.login(dados.usuario,dados.senha)
     });
 
     afterEach(() => {
@@ -18,8 +17,8 @@ context('Funcionalidade Login', () => {
     });
 
     it('Login com sucesso usando Comando customizado', () => {
-        cy.login(dados.usuario,dados.senha)
-        
+        cy.login(dadosLogin.usuario, dadosLogin.senha)
+        cy.get('.page-title').should('contain', 'Minha conta')
     });
 
     it('Login usando fixture', () => {
@@ -34,6 +33,6 @@ context('Funcionalidade Login', () => {
         cy.get('#password').type(dadosLogin.senha, { log: false })
         cy.get('.woocommerce-form > .button').click()
         cy.get('.page-title').should('contain', 'Minha conta')
-        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, Aluno (não é Aluno?')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, Aluno (não é Aluno? Sair)')
     })
 })
